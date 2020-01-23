13ae2c46be2f0fdbbd9c06c4ce93ca20ed69b933
PrayWind <515322309@qq.com>
2020-01-23 13:16:15 +0800

    项目初始化
    配完环境，项目初始化

---
+++ cooperation-server/src/main/java/com/xmut/CooperationApplication.java
@@ -0,0 +1,14 @@
+package com.xmut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CooperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperationApplication.class, args);
+	}
+
+}
